8348a402ce0794892f08c4713098d0cfe36fce32
Brian Boyko <brian.boyko@gmail.com>
2016-10-07 18:49:03 -0500

    Video

--- src/frontend/js/containers/Hero.js
+++ src/frontend/js/containers/Hero.js
@@ -16,6 +16,12 @@ const headline = {
   IS: "HEYRIRÐU Í OKKUR?"
 };
 
+const subheadline = {
+  EN: 'In 2012, by a 2/3ds vote, the Icelandic people told its parliament to enact a constitution "based on" ' +
+  'a constitution a citizen council had drafted. Four years later, they have done nothing. Help us show the politicians who they work for.',
+  IS: 'Árið 2012, með 2 / 3DS atkvæði, íslenska þjóðin sagði þinginu til þess að enact stjórnarskrá "sem byggist á" stjórnarskrá borgari ráðið hafði samið. Fjórum árum síðar, hafa þeir ekki gert neitt. Hjálpið okkur að sýna stjórnmálamenn sem þeir vinna fyrir.'
+};
+
 const styles = StyleSheet.create({
   heroStyle : {
     // height: '100px'
@@ -25,8 +31,15 @@ const styles = StyleSheet.create({
   headlineStyle : {
     color: 'white',
     fontFamily: "Roboto Condensed",
-    fontSize: '13vw',
+    fontWeight: '900',
+    fontSize: '10vw',
     margin: '2vh 4vw'
+  },
+  subheadline: {
+    color: 'white',
+    fontFamily: 'Roboto',
+    fontSize: '2vw',
+    margin: '2vh 4vw',
   },
   flexContainer : {
     display: "flex",
@@ -62,6 +75,9 @@ const styles = StyleSheet.create({
   },
   chartContainer: {
     padding: '0px 30px 0px 30px',
+  },
+  videoContainer: {
+    margin: '2vh, 2vw',
   }
 });
 
@@ -74,15 +90,17 @@ class Hero extends Component {
     return (<div>
       <Paper className={css(styles.heroStyle)} zDepth={1} >
         <div className={css(styles.flexContainer)}>
-        <div ref="leftContainer" className={css(styles.leftContainer)}>
-          <div className={css(styles.headlineStyle)}>
-            {headline[this.props.language]}
+          <div ref="leftContainer" className={css(styles.leftContainer)}>
+            <div className={css(styles.headlineStyle)}>
+              {headline[this.props.language]}
+            </div>
           </div>
-          <div className={css(styles.chartContainer)}>
+          <div ref="rightContainer" className={css(Object.assign(styles.rightContainer))}>
+            <Paper><iframe width="640" height="360" src="https://www.youtube.com/embed/mw2z9lV3W1g" frameborder="0" allowfullscreen></iframe></Paper>
           </div>
         </div>
-        <div className={css(styles.rightContainer)}>
-        </div>
+        <div className={css(styles.subheadline)}>
+          {subheadline[this.props.language]}
         </div>
       </Paper>
       </div>
