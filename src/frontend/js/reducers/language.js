4ba111805ec1330d9f1bd1bcd964cc9d9f66f6c0
GMDIV <mitchelld20@yahoo.com>
2016-10-11 16:50:32 -0500

    added comments and TODOs for each file in reducers

--- src/frontend/js/reducers/language.js
+++ src/frontend/js/reducers/language.js
@@ -2,6 +2,10 @@ import {
   SET_LANGUAGE,
 } from '../constants/index';
 
+
+//TODO: For now, the switch case will not do anything as action is declared
+//as an empty object argument, causing it to always default to return state
+//since  action.type will never exist. - GMDIV
 export function language(state = 'EN', action = {}) {
   switch(action.type){
   case SET_LANGUAGE:
