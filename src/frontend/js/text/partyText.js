6fad023d4a3d62d0d600a8343a92bddbbbde7c4c
Brian Boyko <brian.boyko@gmail.com>
2016-10-12 01:36:29 -0500

    in progress: adding party page

---
+++ src/frontend/js/text/partyText.js
@@ -0,0 +1,36 @@
+// status enum: ":)", ":(", ":|"
+
+export default [
+  {
+    partyName: {
+      EN: "Social Democratic Alliance",
+      IS: "Samfylkingin-Jafnaðarmannaflokkur Íslands"
+    },
+    leader: 'Oddný Guðbjörg Harðardóttir',
+    status: ":)",
+    description: {
+      EN: "The Social Democratic Alliance is a social-democratic political party in Iceland. It is centre-left in alignment.",
+      IS: "Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter "
+    },
+    wiki: {
+      EN: 'https://en.wikipedia.org/wiki/Social_Democratic_Alliance',
+      IS: 'https://is.wikipedia.org/wiki/Samfylkingin'
+    }
+  }
+];
+
+/*
+Bright Future (A list)
+Progressive Party (B list)
+Viðreisn (C list)
+Independence Party (D list)
+Icelandic National Front (E list)
+Flokkur fólksins (F list)
+Social Democratic Alliance (S list)
+Dawn (T list)
+Left-Green Movement (V list)
+Pirate Party (Þ list)
+Parties with a list for only some constituencies
+Humanist Party (H list)
+People's Front of Iceland (R list)
+ */
