9f86481bb9f06d484bf8393072ae15d53b0c744f
Brian Boyko <brian.boyko@gmail.com>
2016-10-12 11:59:57 -0500

    social buttons

--- src/frontend/js/containers/Social.js
+++ src/frontend/js/containers/Social.js
@@ -19,12 +19,8 @@ class Social extends Component {
   render () {
     const mainSocial = (
       <div style={{ display:'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <span style={{marginLeft: '10px', marginRight: '10px'}}>{this.props.language === 'IS' ? "DEILA": "SHARE"}</span>
-          <a href="https://twitter.com/share" className="twitter-share-button" data-text="Join the fight for democracy in Iceland, to fight for democracy in the entire world. " data-url="https://canyouhearus.is" data-hashtags="CanYouHearUs" data-show-count="false">
-            <IconButton>
-              <TwitterIcon width="24px" height="24px"/>
-            </IconButton>
-          </a>
+          <span style={{marginLeft: '10px', marginRight: '10px'}}><a href="https://twitter.com/share" className="twitter-share-button" data-text="Help us fight for #iceland and the #democratic #constitution approved by 2/3rds of voters but blocked by Parliament." data-url="http://canyouhearus.is" data-show-count="false">Tweet</a></span>
+          <span style={{marginLeft: '10px', marginRight: '10px'}}><a href="https://twitter.com/CanYouHearUs_IS" className="twitter-follow-button" data-show-count="false">Follow @CanYouHearUs_IS</a></span>
           <span data-href="https://canyouhearus.is/" data-mobile-iframe="true">
             <a className="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fcanyouhearus.is%2F">
               <IconButton data-href="https://canyouhearus.is/" data-mobile-iframe="true">
@@ -37,7 +33,7 @@ class Social extends Component {
 
     return(
       <div>
-        {this.props.transparent ? mainSocial : (<Paper style={{margin: 'auto', maxWidth:'300px'}} zDepth={3}>{mainSocial}</Paper>)}
+        {this.props.transparent ? mainSocial : (<Paper style={{margin: 'auto', maxWidth:'450px'}} zDepth={3}>{mainSocial}</Paper>)}
       </div>
     );
   }
