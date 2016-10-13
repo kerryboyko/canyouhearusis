aee2de0c6b2be11c20072f701da106a2e5d54441
Brian Boyko <brian.boyko@gmail.com>
2016-10-13 03:58:51 -0500

    black shirt is nine white shirt is ten

--- src/frontend/js/text/learnTexts.js
+++ src/frontend/js/text/learnTexts.js
@@ -4,13 +4,13 @@ export default [
       "EN": ["The new constitution ensures national ownership and sustainable development of Iceland's national resources."],
       "IS": ["Í nýju stjórnarskránni er almenningi tryggð eign á náttúruauðlindum og gerð krafa um að þeim sé ráðstafað með sjálfstæðum hætti og þannig að fullt verð komi fyrir. "],
     },
-    video: "https://www.youtube.com/embed/xUBmMbwjvR8",
+    video: "https://www.youtube.com/embed/eDlIMHfROX4",
   },
   {
     text: {
       "EN": ["The new constitution tackles corruption.", "For example, members of Parliament will be prohibited from participating in any matters which concerns their special and significant interest, or the interests of persons with whom they have close ties.", "They must also disclose information on their financial interests. This is very important in the light of the Panama Papers scandal, which forced Iceland's (now former) Prime Minister to resign this past April, after it was revealed that he and his wife had set up a company through Mossack Fonseca."],
       "IS": ["Nýja stjórnarskráin kveður á um að Alþingismönnum sé óheimilt að taka þátt í meðferð þingmála ef þeir hafa sérstakra og verulegra hagsmuna að gæta eða aðrir sem eru þeim nákomnir. Einnig er tekið fram að alþingismenn beri skyldu til að veita upplýsingar um fjárhagslega hagsmuni sína. Í ljósi uppljóstrana úr Panama-skjölum liggur fyrir að slík ákvæði þurfa að vera í stjónarskrá."],
     },
-    video: "https://www.youtube.com/embed/eDlIMHfROX4",
+    video: "https://www.youtube.com/embed/xUBmMbwjvR8",
   },
 ];
