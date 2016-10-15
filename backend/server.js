48336ff8fbc38c2ea4396f57effd372591389181
Brian Boyko <brian.boyko@gmail.com>
2016-10-14 23:57:34 -0500

    Added gulp support

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ const express = require('express');
 const http = require('http');
 // use the key from the server, or if we're in local dev, from the secrets.js
 // file that I have not shared to Github for obvious reasons.
-const app = express();
+const app = express(); 
 const server = http.Server(app);
 
-const routes = require('./backdist/routes');
+const routes = require('./routes');
 routes.launchRoutes(server, app);
