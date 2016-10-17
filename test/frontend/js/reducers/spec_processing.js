69939d09e07a96db75059f679b817374ea5727d9
Brian Boyko <brian.boyko@gmail.com>
2016-10-16 21:10:16 -0500

    more tests

---
+++ test/frontend/js/reducers/spec_processing.js
@@ -0,0 +1,22 @@
+import chai from 'chai';
+const expect = chai.expect;
+import {
+  setProcessing
+} from '../../../../src/frontend/js/actions/processing';
+import {
+  processing
+} from '../../../../src/frontend/js/reducers/processing';
+
+describe('processing()', () => {
+  it('starts false', () => {
+    expect(processing(undefined, undefined)).to.equal(false);
+  });
+  it('changes to true', () => {
+    expect(processing(undefined, setProcessing(true))).to.equal(true);
+    expect(processing(false, setProcessing(true))).to.equal(true);
+  });
+  it('goes back to false', () => {
+    expect(processing(undefined, setProcessing(false))).to.equal(false);
+    expect(processing(true, setProcessing(false))).to.equal(false);
+  });
+});
