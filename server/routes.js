823e11e637db8ca82e3eadac38ee1ade9b2ef1f7
Brian Boyko <brian.boyko@gmail.com>
2017-09-16 23:34:58 -0500

    Configuration seems to be off

--- server/routes.js
+++ server/routes.js
@@ -1,51 +1,39 @@
 var port = process.env.PORT || 3000;
-var path = require("path");
 var bodyParser = require("body-parser");
 var express = require("express");
 var cors = require("cors");
 var pick = require("lodash.pick");
-var DIST_DIR = path.join(__dirname, "../dist");
 
 var launchRoutes = function(server, app) {
   server.listen(port, function() {
     console.log("Server is listening on port " + port);
   });
   app.use(cors());
-  app.use(
-    bodyParser.json({
-      limit: "100mb"
-    })
-  );
-  app.use(
-    bodyParser.urlencoded({
-      limit: "100mb",
-      extended: true
-    })
-  );
+  app.use(bodyParser.json({
+    limit: "100mb"
+  }));
+  app.use(bodyParser.urlencoded({
+    limit: "100mb",
+    extended: true
+  }));
   // to serve the pages.
-  app.use("/", express.static(path.join(__dirname, "../dist")));
-  app.use("/about", express.static(path.join(__dirname, "../dist")));
-  app.use("/constitution", express.static(path.join(__dirname, "../dist")));
-  app.use("/learn", express.static(path.join(__dirname, "../dist")));
-  app.use("/thankyou", express.static(path.join(__dirname, "../dist")));
-  app.use("/subscribe", express.static(path.join(__dirname, "../dist")));
-  app.use("/parties", express.static(path.join(__dirname, "../dist")));
-  app.use("/pdf", express.static(path.join(__dirname, "../src/pdf")));
-  app.use("/land", express.static(path.join(__dirname, "../dist")));
-  app.use("/is", express.static(path.join(__dirname, "../dist")));
-  app.use("/en", express.static(path.join(__dirname, "../dist")));
-  app.use(
-    "/favicon.ico",
-    express.static(path.join(__dirname, "../src/img/favicon.ico"))
-  );
-  app.use("/img", express.static(path.join(__dirname, "../src/img")));
-
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../dist/index.html"));
-  });
+  app.use("/", express.static("./"));
+  app.use("/dist", express.static("./dist"));
+  app.use("/about", express.static("./"));
+  app.use("/constitution", express.static("./"));
+  app.use("/learn", express.static("./"));
+  app.use("/thankyou", express.static("./"));
+  app.use("/subscribe", express.static("./"));
+  app.use("/parties", express.static("./"));
+  app.use("/pdf", express.static("./src/pdf"));
+  app.use("/land", express.static("./"));
+  app.use("/is", express.static("./"));
+  app.use("/en", express.static("./"));
+  app.use("/favicon.ico", express.static("./src/img/favicon.ico"));
+  app.use("/img", express.static("./src/img"));
 };
-
+,
 module.exports = {
-  default: launchRoutes,
+  default: launchRoutes
   launchRoutes: launchRoutes
-};
+}
