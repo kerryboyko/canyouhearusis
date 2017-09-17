344f33c46a7edca35ff53a07e6c818a7d567fe56
Brian Boyko <brian.boyko@gmail.com>
2017-09-17 00:18:31 -0500

    test

--- server/routes.js
+++ server/routes.js
@@ -43,7 +43,8 @@ var launchRoutes = function(server, app) {
   app.use("/favicon.ico", express.static(path.join(__dirname, "../src/img/favicon.ico")));
   app.use("/img", express.static(path.join(__dirname, "../src/img")));
   app.get("*", function(req, res){
-    res.sendFile(path.join(__dirname, "../dist/index.html"))
+    res.send("Hello World")
+    // res.sendFile(path.join(__dirname, "../dist/index.html"))
   })
 };
 
