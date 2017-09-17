cba79f1d7b6d8ca86b1373cf031efca03675083f
Brian Boyko <brian.boyko@gmail.com>
2017-09-16 23:23:03 -0500

    Server added

--- distServer/routes.js
+++ distServer/routes.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var port = process.env.PORT || 3000;
 var path = require("path");
 var bodyParser = require("body-parser");
@@ -6,22 +8,18 @@ var cors = require("cors");
 var pick = require("lodash.pick");
 var DIST_DIR = path.join(__dirname, "../dist");
 
-var launchRoutes = function(server, app) {
-  server.listen(port, function() {
+var launchRoutes = function launchRoutes(server, app) {
+  server.listen(port, function () {
     console.log("Server is listening on port " + port);
   });
   app.use(cors());
-  app.use(
-    bodyParser.json({
-      limit: "100mb"
-    })
-  );
-  app.use(
-    bodyParser.urlencoded({
-      limit: "100mb",
-      extended: true
-    })
-  );
+  app.use(bodyParser.json({
+    limit: "100mb"
+  }));
+  app.use(bodyParser.urlencoded({
+    limit: "100mb",
+    extended: true
+  }));
   // to serve the pages.
   app.use("/", express.static(path.join(__dirname, "../dist")));
   app.use("/about", express.static(path.join(__dirname, "../dist")));
@@ -34,13 +32,10 @@ var launchRoutes = function(server, app) {
   app.use("/land", express.static(path.join(__dirname, "../dist")));
   app.use("/is", express.static(path.join(__dirname, "../dist")));
   app.use("/en", express.static(path.join(__dirname, "../dist")));
-  app.use(
-    "/favicon.ico",
-    express.static(path.join(__dirname, "../src/img/favicon.ico"))
-  );
+  app.use("/favicon.ico", express.static(path.join(__dirname, "../src/img/favicon.ico")));
   app.use("/img", express.static(path.join(__dirname, "../src/img")));
 
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../dist/index.html"));
   });
 };
